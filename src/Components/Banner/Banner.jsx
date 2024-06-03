0e8b487dfdbc384cace6a44a140b92b53708bfb1
Jamanul <jk.skb.25@gmail.com>
2024-06-03 13:22:17 +0600

    banner done

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -8,9 +8,20 @@ import 'swiper/css/navigation';
 
 import './styles.css';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import useAxiosPublic from '../../Hooks/useAxiosPublic';
+import { useQuery } from '@tanstack/react-query';
 
 
 const Banner = () => {
+    const axiosPublic =useAxiosPublic()
+    const {data: bannerData}=useQuery({
+        queryKey:['bannerData'],
+        queryFn: async ()=>{
+            const res = await axiosPublic.get('/banner')
+            return res.data
+        }
+    })
+    //console.log(bannerData)
     return (
         <div>
             <Swiper
@@ -27,15 +38,17 @@ const Banner = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {
+            bannerData?.map(data=><SwiperSlide className="rounded-3xl" key={data.id}><div className="relative"><img src={data.imageUrl} alt="" />
+            <div className="absolute top-3/4 md:top-auto md:bottom-16 left-1/4">
+                <p className="text-white font-bold md:text-4xl">{data.title}</p>
+                <p className="text-white hidden md:block">{data.description}</p>
+            </div>
+            
+            </div></SwiperSlide>)
+        }
+        
+        
       </Swiper>
 
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Root from './Layout/Root.jsx';
-import Home from './Pages/Home/Home.jsx';
-import AllScholarship from './Pages/AllScholarship/AllScholarship.jsx';
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
+import Root from "./Layout/Root.jsx";
+import Home from "./Pages/Home/Home.jsx";
+import AllScholarship from "./Pages/AllScholarship/AllScholarship.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-    children:[
+    children: [
       {
         path: "/",
-        element: <Home></Home>
+        element: <Home></Home>,
       },
       {
-        path: 'all-scholarship',
-        element:<AllScholarship></AllScholarship>
+        path: "all-scholarship",
+        element: <AllScholarship></AllScholarship>,
       },
-     
-    ]
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
-)
+);
